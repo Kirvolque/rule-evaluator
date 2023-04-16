7866c94cdfd8cd5c1665eead2b3dad427145e45d
Kirill Volkov <kvolque@gmail.com>
2023-04-16 03:51:01 +0200

    * Add scaladoc for RuleEvaluator

--- src/main/scala/ruleevaluator/RuleEvaluator.scala
+++ src/main/scala/ruleevaluator/RuleEvaluator.scala
@@ -10,8 +10,22 @@ import ruleevaluator.token.Token
 import cats.implicits._
 import ruleevaluator.result.Result
 
-
+/**
+ * The `RuleEvaluator` object provides a function to check a set of conditions against a parsed CSV file using an `Evaluator`.
+ *
+ * The `checkRules` function takes in a `RulesFileContent` object, which contains the parsed contents of the rule file, and a `CsvRow`
+ * object, which contains the parsed contents of the CSV file. The function then applies the rules to each row in the CSV file and
+ * returns a `Result` object that contains the evaluation status for each row.
+ */
 object RuleEvaluator {
+
+  /**
+   * Checks the given set of conditions against the parsed CSV file using an `Evaluator`.
+   *
+   * @param conditions the `RulesFileContent` object containing the parsed contents of the rule file
+   * @param parsedCsv  the `CsvRow` object containing the parsed contents of the CSV file
+   * @return a `Result` object containing the evaluation status for each row in the CSV file
+   */
   def checkRules(conditions: RulesFileContent, parsedCsv: CsvRow): Result =
     conditions.lines
       .map(line => parseAndCombineTokens(line, parsedCsv))
