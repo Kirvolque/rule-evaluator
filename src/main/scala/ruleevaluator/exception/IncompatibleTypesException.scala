ba5527d9de5feb07bc939261d2ebfde98dea32a6
Kirill Volkov <kvolque@gmail.com>
2023-04-23 07:12:20 +0200

    * Remove redundant curly brackets

--- src/main/scala/ruleevaluator/exception/IncompatibleTypesException.scala
+++ src/main/scala/ruleevaluator/exception/IncompatibleTypesException.scala
@@ -3,5 +3,5 @@ package ruleevaluator.exception
 import ruleevaluator.token.{Argument, ComparisonOperator, LogicalOperator}
 
 class IncompatibleTypesException(operator: ComparisonOperator, argument1: Argument, argument2: Argument) extends RuntimeException(
-  s"Incompatible types: ${operator} ($argument1, $argument2)"
+  s"Incompatible types: $operator ($argument1, $argument2)"
 )
